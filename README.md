ec26e3f0e5c1a4e0333167adb41ec418a3337c2c
Amr7hmed <Amr7hmed@gmail.com>
2023-10-08 22:31:32 +0200

    update

---
+++ README.md
@@ -0,0 +1,34 @@
+# React Project Web Site
+
+- File types html + js + scss + css .
+- Folder structure:
+  - assets/images : for dynamic images .
+  - assets/style : Contains project style files .
+  - assets/js : It contains the project's action files.
+
+---
+
+- What is this ?
+  - It is a one-page template for a website
+
+---
+
+- Used Bootstrap.
+- Used Jquery.
+- Used Owl Carousel.
+- Used Fontawesome.
+
+---
+
+## How to run loacaly
+
+```
+  1. git clone project
+  2. cd client
+```
+
+## Preview Images From Site
+
+# Home Page
+
+![Home Page Buyer](/assets/image/screenshot/screen-one.png)
---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// owl developer
+var owldeveloper = $(".owl-developer");
+owldeveloper.owlCarousel({
+  loop: false,
+  margin: 15,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    960: {
+      items: 1,
+    },
+    1200: {
+      items: 1,
+    },
+  },
+});
+
+// owl developer
+var owldeveloper = $(".owl-cards");
+owldeveloper.owlCarousel({
+  loop: false,
+  margin: 15,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    960: {
+      items: 2.5,
+    },
+    1200: {
+      items: 4.5,
+    },
+  },
+});
